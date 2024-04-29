6a94ff16f05f0e6dcb96addd88bf137ccafd63a4
omkarjoshi18 <124678187+omkarjoshi18@users.noreply.github.com>
2024-04-29 07:55:52 +0530

    Add files via upload

---
+++ Unix-Lab-Programs-main/Unix-Lab-Programs-main/Message queue Programs/la40/Sender.c
@@ -0,0 +1,30 @@
+// Write a program for chatting between two/three users to demonstrate IPC using message passing (msgget, msgsnd, msgrcv ). 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<string.h>
+
+struct msgbuf {
+    long mtype;
+    char mtext[100];
+};
+
+int main(){
+    int msgid;
+    struct msgbuf msg;
+    key_t key=1234;
+    msgid=msgget(key,0666|IPC_CREAT);
+    msg.mtype=1;
+    while(1){
+        printf("Enter message: ");
+        fgets(msg.mtext,100,stdin);
+        msgsnd(msgid,&msg,sizeof(msg),0);
+        if(strncmp(msg.mtext,"exit",4)==0){
+            break;
+        }
+    }
+    return 0;
+}
